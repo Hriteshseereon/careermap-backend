5a2fd7b47f8a1c59af3ce045a64d553a6af667a4
hriteshseereon <seereon.hritesh@gmail.com>
2026-07-13 12:49:58 +0530

    transaction and bokking mentor details api add

--- src/modules/adminDashboard/adminDashboard.controller.js
+++ src/modules/adminDashboard/adminDashboard.controller.js
@@ -1,5 +1,7 @@
 import {
   getDashboardData,
+  getAllTransactions,
+  getAllMentorBookings,
 } from "./adminDashboard.service.js";
 
 export const getDashboardController =
@@ -14,6 +16,32 @@ async (req, res) => {
 
   } catch (error) {
 
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const getAllTransactionsController = async (req, res) => {
+  try {
+    const result = await getAllTransactions();
+
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const getAllMentorBookingsController = async (req, res) => {
+  try {
+    const result = await getAllMentorBookings();
+
+    res.status(200).json(result);
+  } catch (error) {
     res.status(500).json({
       success: false,
       message: error.message,
